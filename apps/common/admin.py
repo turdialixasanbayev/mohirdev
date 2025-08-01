4d3392c1402377466ed1c844e9cfd0c134d59625
Turdiali <turdialixasanbayev@gmail.com>
2025-08-01 16:49:01 +0500

    start project

---
+++ apps/common/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+
+admin.site.site_header = "mohirdev Admin Paneli"
+admin.site.site_title = "mohirdev Admin Paneli"
+admin.site.index_title = "mohirdev Admin Paneliga Xush Kelibsiz!"
---
+++ readme.md
@@ -0,0 +1,7 @@
+# mohirdev
+
+mohirdev
+
+https://telegra.ph/Mohirdev-clone-loyihasi-08-01
+
+![Mohirdev](image.png)
